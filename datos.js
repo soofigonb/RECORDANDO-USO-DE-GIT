de65142f3c2b663713abee3b7c02aebacc284391
soofigonb <sofiapazgonb@gmail.com>
2024-11-05 11:02:08 -0300

    agregando arrays al archivo datos.js y exportandolos al archivo  index.js

---
+++ datos.js
@@ -0,0 +1,22 @@
+let autos = [
+    "mazda", 
+    "renault",
+    "ferrari", 
+    "mazda",
+    "jeep",
+    "audi",
+    "ferrari"
+];
+
+let animales = [
+    {animal: 'perro', tipo: 'mascota', nombre: 'elvis'},
+    {animal: 'tigre', tipo: 'salvaje', nombre: 'hendrix'},
+    {animal: 'tiburon', tipo: 'salvaje', nombre: 'ray charles'},
+    {animal: 'gato', tipo: 'mascota', nombre: 'lady gaga'}
+];
+
+module.exports = {
+    autos,
+    animales
+};
+
---
+++ index.js
@@ -0,0 +1,7 @@
+const {autos,animales} = require ('./datos');
+
+console.log("\nLista de autos:");
+console.log(autos);
+
+console.log("\nLista de animales:");
+console.log(animales);

cd145176ec75eb2223a4fb4e282439c2e4ca2021
soofigonb <sofiapazgonb@gmail.com>
2024-11-05 11:15:29 -0300

    utilizando lodash para buscar elementos especificos en nuestras arrays

--- index.js
+++ index.js
@@ -6,22 +6,7 @@ const autosUnicos = _.uniq(autos);
 console.log("\nLista de autos únicos:");
 console.log(autosUnicos);
 
-// 2. Usando find() para obtener todos los animales de tipo 'salvaje'
-function obtenerTodosLosAnimalesSalvajes(animalesArray) {
-    let animalesSalvajes = [];
-    let animal;
-    let index = 0;
-
-    // Bucle que busca todos los animales salvajes
-    while ((animal = _.find(animalesArray, { tipo: 'salvaje', index })) !== undefined) {
-        animalesSalvajes.push(animal);
-        index++;
-    }
-
-    return animalesSalvajes;
-}
-
-// Llamar a la función y guardar el resultado
-const animalesSalvajes = obtenerTodosLosAnimalesSalvajes(animales);
-console.log("\nAnimales de tipo salvaje:");
-console.log(animalesSalvajes);
+// 2. Usando find() para obtener el primer animal de tipo 'salvaje'
+const animalSalvaje = _.find(animales, { tipo: 'salvaje' });
+console.log("\nPrimer animal de tipo salvaje:");
+console.log(animalSalvaje);

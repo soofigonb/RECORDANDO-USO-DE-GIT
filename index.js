9506774a78b84c1ddecd319374088409ff4ff618
soofigonb <sofiapazgonb@gmail.com>
2024-11-05 11:08:08 -0300

    utilizando lodash para buscar elementos especificos en nuestras arrays

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ console.log(autos);
 
 console.log("\nLista de animales:");
 console.log(animales);
+
+console.log('-----------------');
+
+const _ = require('lodash');
+const {  autos, animales } = require('./datos');
+
+// Usando uniq() para eliminar duplicados en la lista de autos
+const autosUnicos = _.uniq(autos);
+console.log("\nLista de autos únicos:");
+console.log(autosUnicos);
+
+// Usando find() para buscar un animal específico
+const animalBuscado = _.find(animales, { nombre: 'elvis' });
+console.log("\nAnimal buscado:");
+console.log(animalBuscado);
